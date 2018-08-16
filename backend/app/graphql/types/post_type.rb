cebd7eabfbd2ae4cd2c04b5eb11ab9c5bbb169ef
1Mill <erik.ekberg@outlook.com>
2018-08-16 14:18:10 -0700

    Add description to post

--- backend/app/graphql/types/post_type.rb
+++ backend/app/graphql/types/post_type.rb
@@ -1,6 +1,8 @@
 module Types
   class PostType < Types::BaseObject
-    field :id, ID, null: false
+		description 'A post'
+
+		field :id, ID, null: false
     field :title, String, null: true
   end
 end
