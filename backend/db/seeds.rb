817a916375e7e9e9997dffd6803ebab2d18ea31c
1Mill <erik.ekberg@outlook.com>
2018-08-16 15:28:34 -0700

    Seed database with user information

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,3 +1,10 @@
+rand(3..5).to_i.times do |n|
+	User.create(
+		:name => Faker::HarryPotter.character,
+		:age => [nil, rand(5..100).to_i].sample,
+	)
+end
+
 3.times do |n|
 	Post.create(
 		:title => "Post #{n}"
