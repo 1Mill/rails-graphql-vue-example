c745bc574268aa8cb50679acb919db7380e38d45
1Mill <erik.ekberg@outlook.com>
2018-08-16 14:17:52 -0700

    Get post

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -1,13 +1,13 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+		field :post, PostType, null: true do
+			description 'Find a post by ID or title'
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+			argument :id, ID, required: true
+		end
+
+		def post(id:)
+			Post.find(id)
+		end
   end
 end
