f76775c2cc373a116e928b9d25e29fabe39d4aff
1Mill <erik.ekberg@outlook.com>
2018-08-16 14:28:59 -0700

    Scaffold post object

--- backend/app/graphql/types/post_type.rb
+++ backend/app/graphql/types/post_type.rb
@@ -1,8 +1,9 @@
 module Types
   class PostType < Types::BaseObject
-		description 'A post'
+		description 'An authored post'
 
-		field :id, ID, null: false
-    field :title, String, null: true
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :author, Types::UserType, null: false
   end
 end
