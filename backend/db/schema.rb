17a1c90aa1ab4ac8c3af728ed9b79d9c1824202a
1Mill <erik.ekberg@outlook.com>
2018-08-17 15:18:01 -0700

    Re-generate Post model + migration database

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,15 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_220054) do
+ActiveRecord::Schema.define(version: 2018_08_17_221618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
     t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +31,5 @@ ActiveRecord::Schema.define(version: 2018_08_16_220054) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
