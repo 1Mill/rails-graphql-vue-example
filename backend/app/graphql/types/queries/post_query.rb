dd92658470506d37b681e9d644ed3fd5289881db
1Mill <erik.ekberg@outlook.com>
2018-08-17 15:13:18 -0700

    Formatting
    Remove extra indent

--- backend/app/graphql/types/queries/post_query.rb
+++ backend/app/graphql/types/queries/post_query.rb
@@ -1,13 +1,13 @@
 module Types
 	class Queries::PostQuery < Types::QueryType
-			field :post, Objects::PostType, null: true do
-				description 'Find a post with a given ID'
+		field :post, Objects::PostType, null: true do
+			description 'Find a post with a given ID'
 
-				argument :id, ID, required: true
-			end
+			argument :id, ID, required: true
+		end
 
-			def post(id:)
-				Post.find(id)
-			end
+		def post(id:)
+			Post.find(id)
+		end
 	end
 end
