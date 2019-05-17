25c1a7a0346e1937cc6afe72cf32062377fd0df4
1Mill <erik.ekberg@outlook.com>
2019-05-16 22:00:57 -0700

    Seed database using FactoryBot
    Run `rails db:reset db:seed` to reset and seed the database.

--- src/api/db/seeds/development/blogs.seeds.rb
+++ src/api/db/seeds/development/blogs.seeds.rb
@@ -1,7 +1,3 @@
-12.times do |n|
-	Blog.first_or_create(
-		:content => "Random content to add to blog #{n}",
-		:title => "Blog title #{n}",
-		:slug => "TODO"
-	)
-end
+FactoryBot.create_list(
+	:blog, 12, :seed
+)
--- src/api/spec/factories/blogs.rb
+++ src/api/spec/factories/blogs.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
 	factory :blog do
 		# ! Only include the minimum attributes to create the model.
-		title { 'My blog title' }
-		slug { 'my-blog-title' }
+		title { "My blog title" }
+		slug { "My blog slug" }
+
+		trait :seed do
+			content { ['Content', 'Body', 'Something', 'Else', nil].sample }
+			sequence(:title) { |n| "Some title numbered #{n}" }
+			sequence(:slug) { |n| "TODO-slug-#{n}"}
+		end
 	end
 end
