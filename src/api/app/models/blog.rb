e870ab6efad3c74040e7fca690f0f43e2c25cde7
1Mill <erik.ekberg@outlook.com>
2019-05-26 15:27:39 -0700

    Set slug on blog save

--- src/api/app/models/blog.rb
+++ src/api/app/models/blog.rb
@@ -1,4 +1,10 @@
 class Blog < ApplicationRecord
 	# ! Validations occure on the database level to prevent race
 	# ! conditions. Check out the migration file for more details.
+
+	before_save :set_slug
+
+	private def set_slug
+		self.slug ||= self.title.parameterize
+	end
 end
--- src/api/spec/models/blog_spec.rb
+++ src/api/spec/models/blog_spec.rb
@@ -17,4 +17,55 @@ RSpec.describe Blog, type: :model do
 			end
 		end
 	end
+
+	describe '#slug' do
+		context 'when #slug is not present' do
+			context 'AND when #title is not present' do
+				it 'returns invalid' do
+					expect{ create(:blog, :title => nil, :slug => nil) }.to raise_error
+				end
+			end
+			context 'AND when #title is present' do
+				it 'returns valid' do
+					expect(create(:blog, :title => 'Some title', :slug => nil).persisted?).to eq(true)
+				end
+
+				it 'returns formatted #title' do
+					title = 'Some random title'
+					expected = 'some-random-title'
+
+					blog = create(:blog, :title => title, :slug => nil)
+
+					expect(blog.slug).to eq(expected)
+				end
+			end
+		end
+		context 'when #slug is present' do
+			it 'returns valid' do
+				expect(create(:blog, :slug => 'Some slug').persisted?).to eq(true)
+			end
+
+			it 'returns the input #slug value' do
+				slug = 'some-random-slug'
+
+				blog = create(:blog, :slug => slug)
+
+				expect(blog.slug).to eq(slug)
+			end
+		end
+
+		context 'when #slug is not unique' do
+			it 'returns invalid' do
+				slug = 'some-blog-slug'
+				blog = create(:blog, :slug => slug)
+
+				expect{ create(:blog, :slug => slug) }.to raise_error
+			end
+		end
+		context 'when #slug is unique' do
+			it 'returns valid' do
+				expect(create(:blog, :slug => 'Some unique slug').persisted?).to eq(true)
+			end
+		end
+	end
 end
